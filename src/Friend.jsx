e929277705b32d3313d53762ae73dd0602c059f3
Aminul Haque Ranae <aminulhaquerana1@gmaile.com>
2024-05-22 16:27:06 +0600

    add id

--- src/Friend.jsx
+++ src/Friend.jsx
@@ -2,12 +2,14 @@
 
 
 const Friend = ({bondhu}) => {
-    const {name, email} = bondhu;
+    const {name, email, id} = bondhu;
  
     return (
         <div className="box">
+            <h3>ID: {id} </h3>
             <h3>Name: {name} </h3>
             <h3>Name: {email} </h3>
+            
 
         </div>
     );
